5fed438300eeb8307faa9d76fb1190a87e69564b
Luis Guilherme <guilherme32698@gmail.com>
2025-07-24 16:57:44 -0300

    produzindo menu com leitura de dados, Arraylist comunicando com classe e switch para cada opção do usuário

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leitura = new Scanner(System.in);
+
+        ArrayList<Produto> produtos = new ArrayList<>();
+        int OpcaoUsuario = leitura.nextInt();           
+        System.out.println("\n ---MENU---");
+        System.out.println("\n1 - Cadastrar produtos");
+        System.out.println("\n2 - Listar produtos.");
+        System.out.println("\n3 - Buscar por um produto.");
+        System.out.println("\n4 - Encerrar programa.");
+
+        switch (OpcaoUsuario){
+            case 1:
+            System.out.println("");
+
+        }
+        
+
     }
 }
